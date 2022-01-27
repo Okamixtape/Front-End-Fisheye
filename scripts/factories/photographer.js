7520096e59bd86cfec8de80aa75f7c29842fd9e9
Loup Aubour <laubour@arneogroup.com>
2022-01-27 14:38:24 +0100

    data/photographers.json fetched in order to start homepage info display

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,24 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const h4 = document.createElement( "h4" );
+        h4.textContent = city;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h4);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
