4ed188d99d136483e06215295764f8bd30500338
Loup Aubour <laubour@arneogroup.com>
2022-03-28 22:19:45 +0200

    Ajout attributs aria-label pour le lecteur d'écran VoiceOver

--- scripts/models/photographer.js
+++ scripts/models/photographer.js
@@ -15,15 +15,15 @@ class Photographer {
   photographerCard = () => {
     let cardHTML = `<article class="photographer-card">
                       <div class="card__wrapper">
-                        <a href="./photographer.html?id=${this.id}" aria-label="Portrait photographique de ${this.name}">
+                        <a href="./photographer.html?id=${this.id}" aria-label="Portfolio de ${this.name}">
                           <div class="card__media">
                             <img src="${this.portrait}" alt="${this.name}" class="card__image" />
                           </div>
                           <h2 class="card__title heading__title">${this.name}</h2>
                         </a>
-                        <span class="card__country heading__subtitle" alt="Pays d'origine du photographe" tabindex="0">${this.country}</span>
-                        <p class="card__tagline" alt="Citation du photographe" tabindex="0">${this.tagline}</p>
-                        <span class="card__price" alt="Frais du photographe" tabindex="0">${this.price}&euro;/jour</span>
+                        <span class="card__country heading__subtitle" aria-label="Pays d'origine du photographe" alt="Pays d'origine du photographe" tabindex="0">${this.country}</span>
+                        <p class="card__tagline" aria-label="Citation du photographe" alt="Citation du photographe" tabindex="0">${this.tagline}</p>
+                        <span class="card__price" aria-label="Frais à la journée du photographe" alt="Frais à la journée du photographe" tabindex="0">${this.price}&euro;/jour</span>
                       </div>
                     </article>`
 
@@ -34,10 +34,10 @@ class Photographer {
   photographerInfos = () => {
     let infosHTML = `<div class="infos__details">
                       <h1 class="infos__name heading__title" tabindex="0">${this.name}</h1>
-                      <h2 class="infos__country heading__subtitle" alt="Pays d'origine du photographe" tabindex="0">${this.country}</h2>
-                      <p class="infos__tagline" alt="Citation du photographe" tabindex="0">${this.tagline}</p></div>
+                      <h2 class="infos__country heading__subtitle" aria-label="Pays d'origine du photographe" alt="Pays d'origine du photographe" tabindex="0">${this.country}</h2>
+                      <p class="infos__tagline" aria-label="Citation du photographe" alt="Citation du photographe" tabindex="0">${this.tagline}</p></div>
                       <button class="btn infos__button" aria-label="Contactez le photographe">Contactez-moi</button>
-                      <div class="infos__media"><img src="${this.portrait}" class="infos__image" aria-label="Portrait photographique de ${this.name}" tabindex="0"/>
+                      <div class="infos__media"><img src="${this.portrait}" class="infos__image" aria-label="Portfolio de ${this.name}" tabindex="0"/>
                     </div>`
 
     return infosHTML
@@ -47,8 +47,8 @@ class Photographer {
   photographerDetailsCard = (totalLikes) => {
     let detailsHTML = `<div class="photographer__details">
                       <div class="details__wrapper" tabindex="0">
-                        <p class="details__likes" alt="Nombre total de likes du photographe" tabindex="0">${totalLikes}</p>
-                        <p class="details__price" alt="Frais du photographe" tabindex="0">${this.price}&euro; / jour</p>
+                        <p class="details__likes" aria-label="Nombre total de likes du photographe" alt="Nombre total de likes du photographe" tabindex="0">${totalLikes}</p>
+                        <p class="details__price" aria-label="Frais du photographe à la journée" alt="Frais du photographe à la journée" tabindex="0">${this.price}&euro; / jour</p>
                       </div>
                     </div>`
 
